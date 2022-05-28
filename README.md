68074e291afe512477ae41a1e9daa4cecd04cb70
Arijit Basu <sayanarijit@gmail.com>
2022-05-28 23:07:20 +0530

    Init

---
+++ README.md
@@ -0,0 +1,58 @@
+[![copy.gif](https://s3.gifyu.com/images/copy.gif)](https://gifyu.com/image/tBwL)
+
+Copy and paste with system clipboard using
+[wl-clipboard](https://github.com/bugaevc/wl-clipboard).
+
+## Requirements
+
+- [wl-clipboard](https://github.com/bugaevc/wl-clipboard)
+
+## Installation
+
+### Install using xpm.xplr
+
+```lua
+require("xpm").setup({
+  ---
+  { name = "sayanarijit/wl-clipboard.xplr" }
+  ---
+})
+```
+
+### Install manually
+
+- Add the following line in `~/.config/xplr/init.lua`
+
+  ```lua
+  local home = os.getenv("HOME")
+  package.path = home
+  .. "/.config/xplr/plugins/?/init.lua;"
+  .. home
+  .. "/.config/xplr/plugins/?.lua;"
+  .. package.path
+  ```
+
+- Clone the plugin
+
+  ```bash
+  mkdir -p ~/.config/xplr/plugins
+
+  git clone https://github.com/sayanarijit/wl-clipboard.xplr ~/.config/xplr/plugins/wl-clipboard
+  ```
+
+- Require the module in `~/.config/xplr/init.lua`
+
+  ```lua
+  require("wl-clipboard").setup()
+
+  -- Or
+
+  require("wl-clipboard").setup{
+    copy_command = "wl-copy",
+    paste_command = "wl-paste",
+    keep_selection = false,
+  }
+
+  -- Type `yy` to copy and `p` to paste files.
+  -- Type `yP` to copy the parent directory path.
+  ```
---
+++ init.lua
@@ -0,0 +1,93 @@
+local function setup(args)
+  local xplr = xplr
+
+  args = args or {}
+  args.copy_command = args.copy_command or "wl-copy"
+  args.paste_command = args.paste_command or "wl-paste"
+  args.keep_selection = args.keep_selection or false
+
+  xplr.config.modes.custom.wl_copy = {
+    name = "wl copy",
+    key_bindings = {
+      on_key = {
+        y = {
+          help = "copy files",
+          messages = {
+            {
+              BashExecSilently = [===[
+                if cat ${XPLR_PIPE_RESULT_OUT:?} | ]===] .. args.copy_command .. [===[; then
+                  echo LogSuccess: Copied paths to clipboard >> ${XPLR_PIPE_MSG_IN:?}
+                else
+                  echo LogSuccess: Failed to copy paths >> ${XPLR_PIPE_MSG_IN:?}
+                fi
+              ]===],
+            },
+            "PopMode",
+          },
+        },
+        P = {
+          help = "copy parent directory",
+          messages = {
+            {
+              BashExecSilently = [===[
+              if ]===] .. args.copy_command .. [===[ "${PWD:?}"; then
+                echo "LogSuccess: Copied $PWD to clipboard" >> "${XPLR_PIPE_MSG_IN:?}"
+              else
+                echo "LogError: Failed to copy $PWD" >> "${XPLR_PIPE_MSG_IN:?}"
+              fi
+              ]===],
+            },
+            "PopMode",
+          },
+        },
+        esc = {
+          help = "cancel",
+          messages = {
+            "PopMode",
+          },
+        },
+        ["ctrl-c"] = {
+          messages = {
+            "Terminate",
+          },
+        },
+      },
+    },
+  }
+
+  if not args.keep_selection then
+    table.insert(
+      xplr.config.modes.custom.wl_copy.key_bindings.on_key.y.messages,
+      "ClearSelection"
+    )
+    table.insert(
+      xplr.config.modes.custom.wl_copy.key_bindings.on_key.P.messages,
+      "ClearSelection"
+    )
+  end
+
+  xplr.config.modes.builtin.default.key_bindings.on_key.y = {
+    help = "copy to clipboard",
+    messages = {
+      "PopMode",
+      { SwitchModeCustom = "wl_copy" },
+    },
+  }
+
+  xplr.config.modes.builtin.default.key_bindings.on_key.p = {
+    help = "paste from clipboard",
+    messages = {
+      {
+        BashExec = args.paste_command .. [===[ | while read -r path; do
+            [ -e "$path" ] && cp -var "${path:?}" ./
+          done
+          read -p "[press ENTER to continue]"
+        ]===],
+      },
+      "ExplorePwd",
+      "PopMode",
+    },
+  }
+end
+
+return { setup = setup }
